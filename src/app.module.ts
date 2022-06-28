bbf9dce353a74f6946729119473a7fe2ab0ef4f3
Vinicius <viniholtman28@gmail.com>
2022-06-28 20:49:01 -0300

    Create entity user and alter nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
