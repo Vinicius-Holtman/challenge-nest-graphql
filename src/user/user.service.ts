0e4d6eed9c57e3fa4e4b492157e71c192c44d88c
Vinicius <viniholtman28@gmail.com>
2022-06-28 20:58:11 -0300

    Create service user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { User } from './user.entity';
+
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm'
+import { CreateUserInput } from './dto/create-user-input';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    const user = await this.userRepository.create(data);
+    const userSaved = await this.userRepository.save(user);
+
+    if (!userSaved) {
+      throw new InternalServerErrorException('Problema para criar um usuario')
+    }
+
+    return userSaved;
+  }
+}
